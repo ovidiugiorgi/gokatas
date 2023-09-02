49bbe2c81cf55614c84a4100d59867a97345ce2d
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2023-09-02 22:04:03 +0300

    areader

--- areader/cmd/areader.go
+++ areader/cmd/areader.go
@@ -3,13 +3,22 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/jreisinger/gokatas/areader"
 )
 
 func main() {
 	var a areader.A
-	p := make([]byte, 3)
-	a.Read(p)
-	fmt.Println(string(p))
+	p := make([]byte, 5)
+	n, _ := a.Read(p)
+	if n < len(p) {
+		fmt.Printf("expected areader.A to read %v bytes; got %v\n", len(p), n)
+	}
+	sp := string(p)
+	if sp != strings.Repeat("A", len(p)) {
+		fmt.Printf("expected areader.A to add %v repeated 'A' characters; got %q\n", len(p), sp)
+	} else {
+		fmt.Println(sp)
+	}
 }
